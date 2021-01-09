d9f4d3d1a708f46cf60b72d9efe7b330a2cbbd6a
yousefokka <yousefokka2231@gmail.com>
2021-01-10 00:07:08 +0200

    user commit

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -8,19 +8,13 @@ class Api::V1::SessionsController < ApplicationController
       
       if @user and  @user.valid_password? user_password 
         sign_in @user, store: false
-        @user.generate_authentication_token! 
         @user.save
-        render json: @user.as_json(methods: :user_type), status: 200
+        render json: @user, status: 200
       else
         render json: { errors: "Invalid email or password" }, status: 422
       end   
     end
   
-    def destroy
-      @user = User.find_by(auth_token: params[:id])    
-      @user.generate_authentication_token!
-      @user.destroy
-      head 204
-    end 
+  
   end
   
