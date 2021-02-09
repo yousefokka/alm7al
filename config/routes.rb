3e3d856c2c41516fa852d7279f7aafb8a0e25d1e
yousefokka <yousefokka2231@gmail.com>
2021-02-09 16:55:42 +0200

    time line update

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       namespace :api do
         namespace :v1 do
           resources :sessions, :only => [:create, :destroy] 
-          resources :admins, :only => [:show, :create, :update, :destroy]  do 
+          resources :admins, :only => [:create]  do 
             resources :categories do  
               resources :items do
                 resources :itemimages
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
               resources :carts
               end
           end 
+          resources :categories do   :only => [:show , index] 
+            resources :items do   :only => [:show , index]
         end 
       end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
